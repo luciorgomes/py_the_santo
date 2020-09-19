0ac296d7b53e66edf2aa2d1c134485a44a623220
luciorgomes <luciorgomes@gmail.com>
2020-09-19 12:17:08 -0300

    Combine pdf

--- combine_pdf.py
+++ combine_pdf.py
@@ -5,6 +5,7 @@ import PyPDF2
 import os
 import tkinter as tk
 from tkinter import filedialog
+import glob
 
 
 def combine_pdf():
@@ -18,36 +19,32 @@ def combine_pdf():
     folder = filedialog.askdirectory(parent=directory_root, initialdir='/',
                                      title='Selecione a pasta com os arquivos pdf')
     if not len(folder):  # se clicou 'cancel'
-        cancel = "comando cancelado"
+        cancel = "Comando cancelado."
+        print(cancel)
         return cancel
 
-    os.chdir(folder)  # altera o diretório de trabalho para a pasta 'folder'
+    # altera o diretório de trabalho para a pasta 'folder'
+    os.chdir(folder)
 
-    # Gera lista com a relação de arquivos pdf do diretório
-    pdf_files = []
-    for filename in os.listdir(folder):
-        if filename.endswith('.pdf'):
-            pdf_files.append(filename)
-    print(pdf_files)
+    # gera lista com os nomes dos arquivos do diretório
+    files = [a for a in glob.glob('*.pdf') if a != 'arquivos_concatenados.pdf']
 
-    pdf_files.sort(key=str.lower)
+    # se lista vazia
+    if not len(files):
+        nenhum = 'Nenhum arquivo pdf encontrado'
+        print(nenhum)
+        return nenhum
 
-    pdf_writer = PyPDF2.PdfFileWriter()
+    merger = PyPDF2.PdfFileMerger(strict=False)
 
-    # Percorre os arquivos em um loop
-    for filename in pdf_files:
-        pdf_file_obj = open(filename, 'rb')
-        pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
+    for pdf in files:
+        merger.append(open(pdf, 'rb'))
 
-    # Percorre as páginas e as adiciona à saída
-    for page_num in range(pdf_reader.numPages):
-        page_obj = pdf_reader.getPage(page_num)
-        pdf_writer.addPage(page_obj)
-
-    # Salva o Pdf resultante em um arquivo
-    pdf_output = open('all_files.pdf', 'wb')
-    pdf_writer.write(pdf_output)
-    pdf_output.close()
+    with open("arquivos_concatenados.pdf", "wb") as fout:
+        merger.write(fout)
+    saida = 'Feito! Arqiuvo resultante = "arquivos_concatenados.pdf".'
+    print(saida)
+    return saida
 
 
 if __name__ == '__main__':  # executa se chamado diretamente
