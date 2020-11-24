051760af661a249e567f86ed9316abb7737f3151
luciorgomes <luciorgomes@gmail.com>
2020-11-23 23:31:24 -0300

    21/09/2020

--- mouse_move.py
+++ mouse_move.py
@@ -1,26 +1,23 @@
 import pyautogui
 from pynput import keyboard
-from pynput.mouse import Controller as MouseController
 import random
 
 def move_mouse():
     pyautogui.FAILSAFE = False
 
-    mouse = MouseController()
-
     while True:
-        turn = random.randint(-50, 50)
         with keyboard.Events() as events:
             # Block at most one second
-            event = events.get(1.0)
+            event = events.get(0.5)
             if event is not None:
                 print('Interrompido!')
                 break
             else:
                 move = random.randint(-10, 10)
-                pyautogui.move(- move, - move, duration=.10)
-                pyautogui.move(turn, - turn, duration=.10)
-                pyautogui.moveRel(move, turn, duration=.10)
+                pyautogui.move(move, move, duration=.03)
+                pyautogui.move(- move, move, duration=.03)
+                pyautogui.move(- move, - move, duration=.03)
+                pyautogui.move(move, - move, duration=.03)
 
 
 if __name__ == '__main__':
